7c5306b8c46cb9d2e29b2d77df3d5a723d21bf2e
admin <jessica_qingchen@163.com>
2021-04-09 22:38:05 +0800

    第一次作业

---
+++ testCalc.py
@@ -0,0 +1,29 @@
+# coding:utf-8
+# author:huangjing
+# data:2021-04-09
+import pytest
+
+@pytest.mark.parametrize('x, y', [(1, 2), (0, 6)] )
+class TestCalc:
+    def setup(self):
+        print("-----开始计算------")
+
+    def teardown(self):
+        print("------结束计算------")
+
+    def test_add(self, x, y):
+        print(("x+y=", x + y))
+        return x + y
+
+    def test_mult(self, x, y):
+        print(("x*y=", x * y))
+        return x * y
+
+    def test_division(self, x, y):
+        pytest.xfail(reason='除数不能为0')
+        print("x/y=", x / y)
+        return x / y
+
+
+if __name__ == '__main__':
+    pytest.main(['-s','-r','testCalc.py'])
